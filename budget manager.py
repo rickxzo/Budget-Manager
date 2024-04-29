9c4aa962b8395b56d4f12cdabffe6a54cd643dc9
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2024-04-30 00:21:07 +0530

    Update budget manager.py

--- budget manager.py
+++ budget manager.py
@@ -81,46 +81,46 @@ class Category:
     
     
     def create_spend_chart(categories):
-    category_names = []
-    spent = []
-    spent_percentages = []
+        category_names = []
+        spent = []
+        spent_percentages = []
 
-    for category in categories:
-        total_spent = 0
-        for item in category.ledger:
-            if item['amount'] < 0:
-                total_spent -= item['amount']
-        spent.append(round(total_spent, 2))
-        category_names.append(category.category)
+        for category in categories:
+            total_spent = 0
+            for item in category.ledger:
+                if item['amount'] < 0:
+                    total_spent -= item['amount']
+            spent.append(round(total_spent, 2))
+            category_names.append(category.category)
 
-    for amount in spent:
-        spent_percentages.append(round(amount / sum(spent), 2) * 100)
+        for amount in spent:
+            spent_percentages.append(round(amount / sum(spent), 2) * 100)
 
-    graph = "Percentage spent by category\n"
-    labels = range(100, -10, -10)
+        graph = "Percentage spent by category\n"
+        labels = range(100, -10, -10)
 
-    for label in labels:
-        graph += str(label).rjust(3) + "| "
-        for percent in spent_percentages:
-            if percent >= label:
-                graph += "o  "
-            else:
-                graph += "   "
-        graph += "\n"
+        for label in labels:
+            graph += str(label).rjust(3) + "| "
+            for percent in spent_percentages:
+                if percent >= label:
+                    graph += "o  "
+                else:
+                    graph += "   "
+            graph += "\n"
 
-    graph += "    " + "-" * (3 * len(category_names) + 1) + "\n"
+        graph += "    " + "-" * (3 * len(category_names) + 1) + "\n"
 
-    longest_name_length = max([len(name) for name in category_names])
+        longest_name_length = max([len(name) for name in category_names])
 
-    for i in range(longest_name_length):
-        name_line = "     "
-        for name in category_names:
-            if i < len(name):
-                name_line += name[i] + "  "
-            else:
-                name_line += "   "
-        if i < longest_name_length - 1:
-            name_line += "\n"
-        graph += name_line
+        for i in range(longest_name_length):
+            name_line = "     "
+            for name in category_names:
+                if i < len(name):
+                    name_line += name[i] + "  "
+                else:
+                    name_line += "   "
+            if i < longest_name_length - 1:
+                name_line += "\n"
+            graph += name_line
 
-    return graph
+        return graph
